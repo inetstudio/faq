25f293520428d7b18b4d0b431783535e17247cfc
Konstantin Bukin <bukin@inetstudio.ru>
2018-08-15 16:11:40 +0300

    Change bindings provider

--- entities/questions/src/Providers/QuestionsBindingsServiceProvider.php
+++ entities/questions/src/Providers/QuestionsBindingsServiceProvider.php
@@ -3,33 +3,58 @@
 namespace InetStudio\FAQ\Questions\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Contracts\Foundation\Application;
 
 /**
  * Class QuestionsBindingsServiceProvider.
  */
 class QuestionsBindingsServiceProvider extends ServiceProvider
 {
+    /**
+    * @var  bool
+    */
     protected $defer = true;
 
-    public $bindings = [];
-
     /**
-     * RSSBindingsServiceProvider constructor.
-     *
-     * @param Application $app
-     */
-    public function __construct(Application $app)
-    {
-        parent::__construct($app);
-
-        $this->bindings = getPackageBindings(__DIR__.'/../Contracts');
-    }
+    * @var  array
+    */
+    public $bindings = [
+        'InetStudio\FAQ\Questions\Contracts\Events\Back\ModifyQuestionEventContract' => 'InetStudio\FAQ\Questions\Events\Back\ModifyQuestionEvent',
+        'InetStudio\FAQ\Questions\Contracts\Events\Front\SendQuestionEventContract' => 'InetStudio\FAQ\Questions\Events\Front\SendQuestionEvent',
+        'InetStudio\FAQ\Questions\Contracts\Http\Controllers\Back\QuestionsControllerContract' => 'InetStudio\FAQ\Questions\Http\Controllers\Back\QuestionsController',
+        'InetStudio\FAQ\Questions\Contracts\Http\Controllers\Back\QuestionsDataControllerContract' => 'InetStudio\FAQ\Questions\Http\Controllers\Back\QuestionsDataController',
+        'InetStudio\FAQ\Questions\Contracts\Http\Controllers\Back\QuestionsUtilityControllerContract' => 'InetStudio\FAQ\Questions\Http\Controllers\Back\QuestionsUtilityController',
+        'InetStudio\FAQ\Questions\Contracts\Http\Controllers\Front\QuestionsControllerContract' => 'InetStudio\FAQ\Questions\Http\Controllers\Front\QuestionsController',
+        'InetStudio\FAQ\Questions\Contracts\Http\Requests\Back\SaveQuestionRequestContract' => 'InetStudio\FAQ\Questions\Http\Requests\Back\SaveQuestionRequest',
+        'InetStudio\FAQ\Questions\Contracts\Http\Requests\Front\SaveQuestionRequestContract' => 'InetStudio\FAQ\Questions\Http\Requests\Front\SaveQuestionRequest',
+        'InetStudio\FAQ\Questions\Contracts\Http\Responses\Back\Questions\DestroyResponseContract' => 'InetStudio\FAQ\Questions\Http\Responses\Back\Questions\DestroyResponse',
+        'InetStudio\FAQ\Questions\Contracts\Http\Responses\Back\Questions\FormResponseContract' => 'InetStudio\FAQ\Questions\Http\Responses\Back\Questions\FormResponse',
+        'InetStudio\FAQ\Questions\Contracts\Http\Responses\Back\Questions\IndexResponseContract' => 'InetStudio\FAQ\Questions\Http\Responses\Back\Questions\IndexResponse',
+        'InetStudio\FAQ\Questions\Contracts\Http\Responses\Back\Questions\SaveResponseContract' => 'InetStudio\FAQ\Questions\Http\Responses\Back\Questions\SaveResponse',
+        'InetStudio\FAQ\Questions\Contracts\Http\Responses\Back\Questions\ShowResponseContract' => 'InetStudio\FAQ\Questions\Http\Responses\Back\Questions\ShowResponse',
+        'InetStudio\FAQ\Questions\Contracts\Http\Responses\Back\Utility\ActivityResponseContract' => 'InetStudio\FAQ\Questions\Http\Responses\Back\Utility\ActivityResponse',
+        'InetStudio\FAQ\Questions\Contracts\Http\Responses\Back\Utility\SuggestionsResponseContract' => 'InetStudio\FAQ\Questions\Http\Responses\Back\Utility\SuggestionsResponse',
+        'InetStudio\FAQ\Questions\Contracts\Http\Responses\Front\SaveResponseContract' => 'InetStudio\FAQ\Questions\Http\Responses\Front\SaveResponse',
+        'InetStudio\FAQ\Questions\Contracts\Mail\AnswerMailContract' => 'InetStudio\FAQ\Questions\Mail\AnswerMail',
+        'InetStudio\FAQ\Questions\Contracts\Mail\NewQuestionMailContract' => 'InetStudio\FAQ\Questions\Mail\NewQuestionMail',
+        'InetStudio\FAQ\Questions\Contracts\Models\QuestionModelContract' => 'InetStudio\FAQ\Questions\Models\QuestionModel',
+        'InetStudio\FAQ\Questions\Contracts\Notifications\AnswerNotificationContract' => 'InetStudio\FAQ\Questions\Notifications\AnswerNotification',
+        'InetStudio\FAQ\Questions\Contracts\Notifications\AnswerQueueableNotificationContract' => 'InetStudio\FAQ\Questions\Notifications\AnswerQueueableNotification',
+        'InetStudio\FAQ\Questions\Contracts\Notifications\NewQuestionNotificationContract' => 'InetStudio\FAQ\Questions\Notifications\NewQuestionNotification',
+        'InetStudio\FAQ\Questions\Contracts\Notifications\NewQuestionQueueableNotificationContract' => 'InetStudio\FAQ\Questions\Notifications\NewQuestionQueueableNotification',
+        'InetStudio\FAQ\Questions\Contracts\Observers\QuestionObserverContract' => 'InetStudio\FAQ\Questions\Observers\QuestionObserver',
+        'InetStudio\FAQ\Questions\Contracts\Repositories\QuestionsRepositoryContract' => 'InetStudio\FAQ\Questions\Repositories\QuestionsRepository',
+        'InetStudio\FAQ\Questions\Contracts\Services\Back\QuestionsDataTableServiceContract' => 'InetStudio\FAQ\Questions\Services\Back\QuestionsDataTableService',
+        'InetStudio\FAQ\Questions\Contracts\Services\Back\QuestionsObserverServiceContract' => 'InetStudio\FAQ\Questions\Services\Back\QuestionsObserverService',
+        'InetStudio\FAQ\Questions\Contracts\Services\Back\QuestionsServiceContract' => 'InetStudio\FAQ\Questions\Services\Back\QuestionsService',
+        'InetStudio\FAQ\Questions\Contracts\Services\Front\QuestionsServiceContract' => 'InetStudio\FAQ\Questions\Services\Front\QuestionsService',
+        'InetStudio\FAQ\Questions\Contracts\Transformers\Back\QuestionTransformerContract' => 'InetStudio\FAQ\Questions\Transformers\Back\QuestionTransformer',
+        'InetStudio\FAQ\Questions\Contracts\Transformers\Back\SuggestionTransformerContract' => 'InetStudio\FAQ\Questions\Transformers\Back\SuggestionTransformer',
+    ];
 
     /**
      * Получить сервисы от провайдера.
      *
-     * @return array
+     * @return  array
      */
     public function provides()
     {
--- entities/tags/src/Providers/TagsBindingsServiceProvider.php
+++ entities/tags/src/Providers/TagsBindingsServiceProvider.php
@@ -3,33 +3,49 @@
 namespace InetStudio\FAQ\Tags\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Contracts\Foundation\Application;
 
 /**
  * Class TagsBindingsServiceProvider.
  */
 class TagsBindingsServiceProvider extends ServiceProvider
 {
+    /**
+    * @var  bool
+    */
     protected $defer = true;
 
-    public $bindings = [];
-
     /**
-     * RSSBindingsServiceProvider constructor.
-     *
-     * @param Application $app
-     */
-    public function __construct(Application $app)
-    {
-        parent::__construct($app);
-
-        $this->bindings = getPackageBindings(__DIR__.'/../Contracts');
-    }
+    * @var  array
+    */
+    public $bindings = [
+        'InetStudio\FAQ\Tags\Contracts\Events\Back\ModifyTagEventContract' => 'InetStudio\FAQ\Tags\Events\Back\ModifyTagEvent',
+        'InetStudio\FAQ\Tags\Contracts\Http\Controllers\Back\TagsControllerContract' => 'InetStudio\FAQ\Tags\Http\Controllers\Back\TagsController',
+        'InetStudio\FAQ\Tags\Contracts\Http\Controllers\Back\TagsDataControllerContract' => 'InetStudio\FAQ\Tags\Http\Controllers\Back\TagsDataController',
+        'InetStudio\FAQ\Tags\Contracts\Http\Controllers\Back\TagsUtilityControllerContract' => 'InetStudio\FAQ\Tags\Http\Controllers\Back\TagsUtilityController',
+        'InetStudio\FAQ\Tags\Contracts\Http\Requests\Back\SaveTagRequestContract' => 'InetStudio\FAQ\Tags\Http\Requests\Back\SaveTagRequest',
+        'InetStudio\FAQ\Tags\Contracts\Http\Responses\Back\Tags\DestroyResponseContract' => 'InetStudio\FAQ\Tags\Http\Responses\Back\Tags\DestroyResponse',
+        'InetStudio\FAQ\Tags\Contracts\Http\Responses\Back\Tags\FormResponseContract' => 'InetStudio\FAQ\Tags\Http\Responses\Back\Tags\FormResponse',
+        'InetStudio\FAQ\Tags\Contracts\Http\Responses\Back\Tags\IndexResponseContract' => 'InetStudio\FAQ\Tags\Http\Responses\Back\Tags\IndexResponse',
+        'InetStudio\FAQ\Tags\Contracts\Http\Responses\Back\Tags\SaveResponseContract' => 'InetStudio\FAQ\Tags\Http\Responses\Back\Tags\SaveResponse',
+        'InetStudio\FAQ\Tags\Contracts\Http\Responses\Back\Tags\ShowResponseContract' => 'InetStudio\FAQ\Tags\Http\Responses\Back\Tags\ShowResponse',
+        'InetStudio\FAQ\Tags\Contracts\Http\Responses\Back\Utility\SuggestionsResponseContract' => 'InetStudio\FAQ\Tags\Http\Responses\Back\Utility\SuggestionsResponse',
+        'InetStudio\FAQ\Tags\Contracts\Models\TaggableModelContract' => 'InetStudio\FAQ\Tags\Models\TaggableModel',
+        'InetStudio\FAQ\Tags\Contracts\Models\TagModelContract' => 'InetStudio\FAQ\Tags\Models\TagModel',
+        'InetStudio\FAQ\Tags\Contracts\Observers\TagObserverContract' => 'InetStudio\FAQ\Tags\Observers\TagObserver',
+        'InetStudio\FAQ\Tags\Contracts\Repositories\TaggablesRepositoryContract' => 'InetStudio\FAQ\Tags\Repositories\TaggablesRepository',
+        'InetStudio\FAQ\Tags\Contracts\Repositories\TagsRepositoryContract' => 'InetStudio\FAQ\Tags\Repositories\TagsRepository',
+        'InetStudio\FAQ\Tags\Contracts\Services\Back\TagsDataTableServiceContract' => 'InetStudio\FAQ\Tags\Services\Back\TagsDataTableService',
+        'InetStudio\FAQ\Tags\Contracts\Services\Back\TagsObserverServiceContract' => 'InetStudio\FAQ\Tags\Services\Back\TagsObserverService',
+        'InetStudio\FAQ\Tags\Contracts\Services\Back\TagsServiceContract' => 'InetStudio\FAQ\Tags\Services\Back\TagsService',
+        'InetStudio\FAQ\Tags\Contracts\Services\Front\TagsServiceContract' => 'InetStudio\FAQ\Tags\Services\Front\TagsService',
+        'InetStudio\FAQ\Tags\Contracts\Transformers\Back\SuggestionTransformerContract' => 'InetStudio\FAQ\Tags\Transformers\Back\SuggestionTransformer',
+        'InetStudio\FAQ\Tags\Contracts\Transformers\Back\TagTransformerContract' => 'InetStudio\FAQ\Tags\Transformers\Back\TagTransformer',
+    ];
 
     /**
      * Получить сервисы от провайдера.
      *
-     * @return array
+     * @return  array
      */
     public function provides()
     {
